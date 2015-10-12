817371e3448d511f8090a05bea26dbabd7d0e31e
kelostrada <kelostrada@gmail.com>
2015-10-12 12:47:41 +0200

    added nuspec so the package can be uploaded to nuget

--- src/TwoFactor/Properties/AssemblyInfo.cs
+++ src/TwoFactor/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TwoFactor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Google authentication using RFC 4226 and RFC 6238")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Rick Bassham")]
 [assembly: AssemblyProduct("TwoFactor")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Rick Bassham 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
